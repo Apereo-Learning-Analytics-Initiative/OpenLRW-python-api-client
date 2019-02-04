c8590b4f3aa8c5a68a9f313eef58b9739b68089c
xchopin <xchopin54@gmail.com>
2019-02-04 16:33:09 +0100

    chore: add requirement

--- setup.py
+++ setup.py
@@ -50,5 +50,5 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    ),
+    ), requires=['requests']
 )
