242084beca0981081b2e7797668e69645763c792
Xavier Chopin <xchopin54@gmail.com>
2019-05-16 11:10:42 +0200

    fix: requirements key

--- setup.py
+++ setup.py
@@ -50,5 +50,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    ], requires=['requests']
+    ], 
+    install_requires=['requests']
 )
